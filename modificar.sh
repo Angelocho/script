f20224c257ded2cb021e9da119cad0b81bb8edb5
Angel Bocalandro Windows <angelocho64@gmail.com>
2021-11-22 18:24:56 +0100

    script modificar.sh

--- modificar.sh
+++ modificar.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+read -p "Dame el nombre " nombre
+read -p "Dame el mail " mail
+password=$(openssl rand -base64 12)
+nombre=$(echo $nombre | tr '[:upper:]' '[:lower:]')
+
+if [[ -z "$nombre" ]]
+then
+        echo " el nombre está vacio" 
+      elif id $nombre;  then
+        echo "$nombre:$password" | chpasswd
+        echo "Tu NUEVA contraseña de usuario para conectarte por sftp es $password y tu usuario $nombre" | mail -s "New Account Password $nombre" $mail >&/dev/null
+        echo "Contraseña modificada"
+       else
+           echo "Usuario no existe"
+
+fi
